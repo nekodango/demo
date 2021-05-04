b9387594089f81b4c659ea160bbf65150f7fb130
nekodango <nekodango@mac.com>
2021-05-04 22:54:04 +0900

    Holaに変更

--- hello-cloudbuild/main.go
+++ hello-cloudbuild/main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+	fmt.Fprintln(w, "Hola, 世界")
 }
+
 
 func main() {
 	http.HandleFunc("/", handler)
